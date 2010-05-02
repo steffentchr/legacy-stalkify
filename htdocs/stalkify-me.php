1eb576b11449cfe1739e101dfd86ce670dd5a162
Steffen Tiedemann Christensen <steffen@refresh.dk>
2010-05-02 09:13:17 +0000

    stalkify me help page

---
+++ htdocs/stalkify-me.php
@@ -0,0 +1,20 @@
+<? require('header.php'); ?>
+
+<h3>How do I get stalked?</h3>
+<p>Stalkify works with everything that speak <a href="http://last.fm">Last.fm</a> &minus; so if you want to be stalked, you simply need to set your Winamp, your iTunes, your Spotify or your whatever up to tell Last.fm about the tracks you're playing.</p>
+
+
+<h3>Spotify Desktop?</h3>
+<p>Spotify can be made to lastfmify your tracks under <i>Preferences</i>:</p>
+<p><img src="/spotify-desktop.png" /></p>
+
+
+<h3>Spotify Mobile?</h3>
+<p>So can Spotify Mobile under <i>More</i> or <i>Settings</i>:</p>
+<p><img src="/spotify-mobile.png" /></p>
+
+
+<h3>What else?</h3>
+<p>It's almost a certainty that your favorite music player is stalkable. Head over to <a href="http://www.last.fm/download" target="_new">Last.fm</a> to learn how.
+
+<? require('footer.php'); ?>
